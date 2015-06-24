1fffde36337cab2e136031fe566a67cac22861c2
Dale Wijnand <dale.wijnand@gmail.com>
2015-06-24 15:35:01 +0100

    Minor cleanup.

--- src/main/scala/playping/controllers/RootController.scala
+++ src/main/scala/playping/controllers/RootController.scala
@@ -1,7 +1,6 @@
 package playping
 package controllers
 
-import play.api._
 import play.api.mvc._
 
 class RootController extends Controller {
--- src/main/scala/playping/package.scala
+++ src/main/scala/playping/package.scala
@@ -5,7 +5,6 @@ import java.time.ZonedDateTime
 
 package object playping
   extends PlayImplicits {
-
 }
 
 package playping {
