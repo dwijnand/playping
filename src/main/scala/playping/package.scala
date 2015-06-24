7ee356b9d7922b779c104770597b0784dbbe54df
Dale Wijnand <dale.wijnand@gmail.com>
2015-06-24 13:31:53 +0100

    Add Writeable[ZonedDateTime].

--- src/main/scala/playping/package.scala
+++ src/main/scala/playping/package.scala
@@ -1,5 +1,16 @@
-package object playping {
+import play.api.http.Writeable
+import play.api.mvc.Codec
+
+import java.time.ZonedDateTime
+
+package object playping
+  extends PlayImplicits {
+
 }
 
 package playping {
+  trait PlayImplicits {
+    implicit def zonedDateTimeWritable(implicit codec: Codec): Writeable[ZonedDateTime] =
+      implicitly[Writeable[String]].map[ZonedDateTime](_.toString)
+  }
 }
