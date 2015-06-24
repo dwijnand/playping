779a9b6e05c2f4ea063764f46cbaa946fba66b6a
Dale Wijnand <dale.wijnand@gmail.com>
2015-06-24 13:32:17 +0100

    Add /datetime.

--- src/main/scala/playping/controllers/RootController.scala
+++ src/main/scala/playping/controllers/RootController.scala
@@ -5,5 +5,6 @@ import play.api._
 import play.api.mvc._
 
 class RootController extends Controller {
-  def ping = Action(Ok("pong"))
+  def ping     = Action(Ok("pong"))
+  def datetime = Action(Ok(java.time.ZonedDateTime.now()))
 }
