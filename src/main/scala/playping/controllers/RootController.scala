5d5723f7eaf0690ae9970ceb8408fa59c8843e3e
Dale Wijnand <dale.wijnand@gmail.com>
2015-06-24 13:32:24 +0100

    Add /boom.

--- src/main/scala/playping/controllers/RootController.scala
+++ src/main/scala/playping/controllers/RootController.scala
@@ -7,4 +7,5 @@ import play.api.mvc._
 class RootController extends Controller {
   def ping     = Action(Ok("pong"))
   def datetime = Action(Ok(java.time.ZonedDateTime.now()))
+  def boom     = Action(Ok(sys error "Boom"))
 }
